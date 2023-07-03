3b9a090d63cadf15909cfa151a717c0a1714489f
Sakshi Shinde <sakshishinde1749@gmail.com>
2023-07-04 01:54:28 +0530

    xylophone app added

---
+++ android/app/src/main/kotlin/sakshi/com/xylophone/MainActivity.kt
@@ -0,0 +1,6 @@
+package sakshi.com.xylophone
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
+
+void main() {
+  runApp(const XylophoneApp());
+}
+
+class XylophoneApp extends StatelessWidget {
+  const XylophoneApp({super.key});
+
+  Future<void> playSound(int soundNumber) async {
+    final player = AudioPlayer();
+    await player.play(AssetSource('note$soundNumber.wav'));
+  }
+
+  Expanded buildKey({required Color color, required int soundNumber}){
+    return Expanded(
+      child: Container(
+        color: color,
+        child: TextButton(
+          onPressed: (){
+            playSound(soundNumber);
+          },
+          child: const Text(''),
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.black,
+        body: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+
+              buildKey(color: Colors.red, soundNumber: 1),
+              buildKey(color: Colors.orange, soundNumber: 2),
+              buildKey(color: Colors.yellow, soundNumber: 3),
+              buildKey(color: Colors.green, soundNumber: 4),
+              buildKey(color: Colors.teal, soundNumber: 5),
+              buildKey(color: Colors.blue, soundNumber: 6),
+              buildKey(color: Colors.purple, soundNumber: 7),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
